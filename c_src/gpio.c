b3bed234f5abc17b6adb0f00503fa17ec69e5b6a
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-10-29 18:05:13 +0100

    GPIO.

--- c_src/gpio.c
+++ c_src/gpio.c
@@ -1,148 +1,170 @@
-#include <wiringPiSPI.h>
+#include <wiringPi.h>
+#include <softPwm.h>
 #include <erl_nif.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
 
+#define PINS 17
+#define HW_PWM 1
+#define HW_PWM_RANGE 1024
+#define SOFT_PWM_RANGE 100
+
 typedef struct {
   ErlNifEnv *env;
-  int fd;
-  unsigned long speed;
+  int pin_pwm_enabled[PINS];
 } state_t;
 
 extern int errno;
 
-static state_t state0;
-static state_t state1;
+static state_t state;
 
-static ERL_NIF_TERM setup_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-	unsigned int channel;
-	unsigned long speed;
-	int error;
-
-	if (argc != 2 || !enif_is_number(env, argv[0]) || !enif_is_number(env, argv[1])) {
-		return enif_make_badarg(env);
-	}
-	if (!enif_get_uint(env, argv[0], &channel)) {
-		return enif_make_badarg(env);
-	}
-	if (!enif_get_ulong(env, argv[1], &speed)) {
-		return enif_make_badarg(env);
-	}
-	if (speed < 500000 || speed > 32000000) {
-		return enif_make_badarg(env);
-	}
-	switch (channel) {
-		case 0:
-			if (state0.fd != 0) {
-				return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_string(env, "channel already opened", ERL_NIF_LATIN1));
-			}
-			else {
-				state0.env = env;
-				state0.fd = wiringPiSPISetup(channel, speed);
-				if (state0.fd == 0) {
-					error = errno;
-					return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_int(env, error));
-				}
-				else {
-					return enif_make_tuple2(env, enif_make_atom(env, "ok"), enif_make_int(env, channel));
-				}
-			}
-			break;
-		case 1:
-			if (state1.fd != 0) {
-				return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_string(env, "channel already opened", ERL_NIF_LATIN1));
-			}
-			else {
-				state1.env = env;
-				state1.fd = wiringPiSPISetup(channel, speed);
-				if (state1.fd == 0) {
-					error = errno;
-					return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_int(env, error));
-				}
-				else {
-					return enif_make_tuple2(env, enif_make_atom(env, "ok"), enif_make_int(env, channel));
-				}
-			}
-			break;
-		default:
-			return enif_make_badarg(env);
-	}
+static int map(int x, int in_min, int in_max, int out_min, int out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-static ERL_NIF_TERM close_1(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-    unsigned int channel;
+static ERL_NIF_TERM setup_0(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+	int error, i;
 
-	if (argc != 1 || !enif_is_number(env, argv[0])) {
-    	return enif_make_badarg(env);
+	error = wiringPiSetup();
+	if (error != 0) {
+	    error = errno;
+	    return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_int(env, error));
+	}
+	state.env = env;
+	for (i = 0; i < PINS; ++i) {
+	    state.pin_pwm_enabled[i] = 0;
+	}
+	state.pin_pwm_enabled[HW_PWM] = 1;
+	
+	return enif_make_atom(env, "ok");
+}
+
+static ERL_NIF_TERM pin_mode_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    int pin, mode;
+    
+    if (argc != 2 || !enif_is_number(env, argv[0]) || !enif_is_number(env, argv[1])) {
+        return enif_make_badarg(env);
     }
-	if (!enif_get_uint(env, argv[0], &channel)) {
-		return enif_make_badarg(env);
-	}
-	switch (channel) {
-		case 0:
-			if (state0.fd != 0) {
-				close(state0.fd);
-				state0.fd = 0;
-			}
-			break;
-		case 1:
-			if (state1.fd != 0) {
-				close(state1.fd);
-				state1.fd = 0;
-			}
-			break;
-		default:
-			return enif_make_badarg(env);
-	}
+    if (!enif_get_int(env, argv[0], &pin)) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[1], &mode)) {
+        return enif_make_badarg(env);
+    }
+    if (pin < 0 || pin > PINS - 1) {
+        return enif_make_badarg(env);
+    }
+    switch (mode) {
+        case INPUT:
+            pinMode(pin, INPUT);
+            break;
+        case OUTPUT:
+            pinMode(pin, OUTPUT);
+            break;
+        case PWM_OUTPUT:
+            pinMode(pin, PWM_OUTPUT);
+            break;
+        default:
+            enif_make_badarg(env);
+            break;
+    }
     
     return enif_make_atom(env, "ok");
 }
 
-static ERL_NIF_TERM xfer_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-    unsigned int channel, length;
-    int result, i, val;
-    uint8_t *buffer;
-    ERL_NIF_TERM cell, head, tail, list;
-
-	if (argc < 2 || !enif_is_number(env, argv[0]) || !enif_is_list(env, argv[1])) {
-    	return enif_make_badarg(env);
+static ERL_NIF_TERM digital_write_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    int pin, value;
+    
+    if (argc != 2 || !enif_is_number(env, argv[0]) || !enif_is_number(env, argv[1])) {
+        return enif_make_badarg(env);
     }
-    if (!enif_get_uint(env, argv[0], &channel) || channel < 0 || channel > 1) {
-    	return enif_make_badarg(env);
+    if (!enif_get_int(env, argv[0], &pin)) {
+        return enif_make_badarg(env);
     }
-    if (!enif_get_list_length(env, argv[1], &length) || length == 0) {
-    	return enif_make_badarg(env);
+    if (!enif_get_int(env, argv[1], &value)) {
+        return enif_make_badarg(env);
     }
-    buffer = (uint8_t *) enif_alloc(sizeof(uint8_t) * length);
-    list = argv[1];
-	for (i = 0; enif_get_list_cell(env, list, &head, &tail); ++i, list = tail) {
-		if (!enif_get_int(env, head, &val)) {
-			return enif_make_badarg(env);
-		}
-		buffer[i] = val;
-	}
-	result = wiringPiSPIDataRW(channel, buffer, length);
-	if (result == -1) {
-		result = errno;
-		enif_free(buffer);
-		return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_int(env, result));
-	}
-	list = enif_make_list(env, 0);
-	for (i = length - 1; i >= 0; --i) {
-		cell = enif_make_uint(env, (unsigned int) buffer[i]);
-		list = enif_make_list_cell(env, cell, list);
-	}
-	enif_free(buffer);
-
-	return enif_make_tuple2(env, enif_make_atom(env, "ok"), list);
+    if (pin < 0 || pin > PINS - 1) {
+        return enif_make_badarg(env);
+    }
+    switch (value) {
+        case LOW:
+            digitalWrite(pin, LOW);
+            break;
+        case HIGH:
+            digitalWrite(pin, HIGH);
+            break;
+        default:
+            enif_make_badarg(env);
+            break;
+    }
+    
+    return enif_make_atom(env, "ok");
 }
 
-static ErlNifFunc spidev_NIFs[] = {
-    {"xfer", 2, &xfer_2},
-    {"setup", 2, &setup_2},
-    {"close", 1, &close_1}
+static ERL_NIF_TERM analog_write_2(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    int pin, value, error;
+    
+    if (argc != 2 || !enif_is_number(env, argv[0]) || !enif_is_number(env, argv[1])) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[0], &pin)) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[1], &value)) {
+        return enif_make_badarg(env);
+    }
+    if (pin < 0 || pin > PINS - 1) {
+        return enif_make_badarg(env);
+    }
+    if (value < 0 || value > HW_PWM_RANGE) {
+        return enif_make_badarg(env);
+    }
+    if (pin == HW_PWM) {
+        pwmWrite(pin, value);
+    }
+    else {
+        if (state.pin_pwm_enabled[pin] == 0) {
+            error = softPwmCreate(pin, 0, SOFT_PWM_RANGE);
+            if (error != 0) {
+                error = errno;
+                return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_int(env, error));
+            }
+            state.pin_pwm_enabled[pin] = 1;
+        }
+        value = map(value, 0, HW_PWM_RANGE, 0, SOFT_PWM_RANGE);
+        softPwmWrite(pin, value);
+    }
+    
+    return enif_make_atom(env, "ok");
+}
+
+static ERL_NIF_TERM digital_read_1(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    int pin, res;
+    
+    if (argc != 1 || !enif_is_number(env, argv[0])) {
+        return enif_make_badarg(env);
+    }
+    if (!enif_get_int(env, argv[0], &pin)) {
+        return enif_make_badarg(env);
+    }
+    if (pin < 0 || pin > PINS - 1) {
+        return enif_make_badarg(env);
+    }
+    res = digitalRead(pin);
+    
+    return enif_make_int(env, res);
+}
+
+static ErlNifFunc gpio_NIFs[] = {
+    {"setup", 0, &setup_0},
+    {"pin_mode", 2, &pin_mode_2},
+    {"digital_write", 2, &digital_write_2},
+    {"digital_read", 1, &digital_read_1},
+    {"analog_write", 2, &analog_write_2}
 };
 
-ERL_NIF_INIT(spidev, spidev_NIFs, NULL, NULL, NULL, NULL);
+ERL_NIF_INIT(gpio, gpio_NIFs, NULL, NULL, NULL, NULL);
--- src/gpio.erl
+++ src/gpio.erl
@@ -1,5 +1,5 @@
 -module(gpio).
--export([init/0, setup/0, pin_mode/2, digital_write/2, digital_read/2, analog_write/2, register_interrupt/3]).
+-export([init/0, setup/0, pin_mode/2, digital_write/2, digital_read/1, analog_write/2]).
 -on_load(init/0).
 
 -define(APPNAME, gpio).
@@ -22,11 +22,8 @@ pin_mode(_Pin, _Mode) ->
 digital_write(_Pin, _Value) ->
     erlang:nif_error(nif_not_loaded).
 
-digital_read(_Pin, _Value) ->
+digital_read(_Pin) ->
     erlang:nif_error(nif_not_loaded).
 
 analog_write(_Pin, _Value) ->
-    erlang:nif_error(nif_not_loaded).
-
-register_interrupt(_Pid, _Pin, _EdgeType) ->
     erlang:nif_error(nif_not_loaded).
